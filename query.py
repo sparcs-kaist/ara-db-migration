8bba9c902520d28d3d52fc7fbcd27d4091c30a30
victory-jooyon <gina611.kaist@gmail.com>
2020-10-30 11:47:23 +0900

    Add last_updated_at in article

--- query.py
+++ query.py
@@ -90,8 +90,9 @@ write_queries = {
     'core_article_consecutive': """insert into core_article(id, created_at, updated_at, deleted_at, title, content, content_text,
                     is_anonymous, is_content_sexual, is_content_social, hit_count,
                     positive_vote_count, negative_vote_count, commented_at, created_by_id, parent_board_id,
-                    parent_topic_id, url, migrated_hit_count, migrated_positive_vote_count, migrated_negative_vote_count, comment_count)
-                    values (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)""",
+                    parent_topic_id, url, migrated_hit_count, migrated_positive_vote_count, migrated_negative_vote_count, comment_count,
+                    last_updated_at)
+                    values (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)""",
     'core_comment_consecutive': """insert into core_comment(id, created_at, updated_at, deleted_at, content,
                      is_anonymous, positive_vote_count, negative_vote_count, attachment_id, 
                      created_by_id, parent_article_id, parent_comment_id)
